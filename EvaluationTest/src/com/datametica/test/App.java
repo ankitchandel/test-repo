43e20c8ad32652859887a4aae78b718cb9d1bad7
ankitchandel <shaanchandel@gmail.com>
2015-09-10 18:35:11 +0530

    program done

--- EvaluationTest/src/com/datametica/test/App.java
+++ EvaluationTest/src/com/datametica/test/App.java
@@ -4,7 +4,24 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			for(int i=1;i<=6;++i)
+		{
+		for(int j=i;j<6;++j)
+		{
+		System.out.print(" ");
+		}
+		for(int k=1;k<=i;++k)
+		{
+		System.out.print("*");
+		}
+		System.out.println();
+		}
 
-	}
 
-}
+		}
+		}
+
+
+	
+
+
